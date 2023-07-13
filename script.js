274183af741ad97a70325fee7e4f862aa72a808d
Iftekhar16 <iftekharhossain16@gmail.com>
2023-07-14 03:16:40 +0600

    probably done?

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+
+document.getElementById('search').addEventListener("keypress", function(event){
+    if(event.key === "Enter"){
+        document.getElementById('btn-search').click();
+    }
+})
+
+
 function loadMeals(){
-    let searchResult = document.getElementById('btn-search').value
+    let searchResult = document.getElementById('search').value
     if(searchResult != ''){
         // let searchResult = 'fish'
         // let apiLink = 'https://www.themealdb.com/api/json/v1/1/search.php?s=fish'
@@ -9,34 +17,28 @@ function loadMeals(){
         .then(res => res.json())
         .then(data => doStuff(data));
     }
-
-
 }
 
 function doStuff(data){
 
-    for (i in data.meals){
-    //     console.log(data.meals[i].strMeal);
+    document.getElementById('empty-text').innerHTML = ''
+    document.getElementById('wrapper').innerHTML = ''
 
-    //     document.getElementById('wrapper').innerHTML += `
-    //         <img src="${data.meals[i].strMealThumb}" alt="No image available">
-    //         <h1 class="name">${data.meals[i].strMeal}</h1>
-    //         <div class="description">${data.meals[i].strArea}, ${data.meals[i].strCategory}</div>
-    //     `
-    // }
-
-
-
-        document.getElementById('wrapper').innerHTML += `
-            <div class="food-item">
-                <img src="${data.meals[i].strMealThumb}" alt="No image available" class="demo-picture w-full h-[300px] bg-black object-cover"></img>
-                <div class="name pl-3 pt-2 text-2xl">${data.meals[i].strMeal}</div>
-                <div class="description pl-3 pb-3">${data.meals[i].strArea}, ${data.meals[i].strCategory}</div>
-            </div>
-        `
+    if (Array.isArray(data.meals) && data.meals.length) {
+        for (i in data.meals){
+            document.getElementById('wrapper').innerHTML += `
+                <div class="food-item border border-color6 bg-color4 hover:shadow-2xl transition">
+                    <img src="${data.meals[i].strMealThumb}" alt="No image available" class="demo-picture w-full h-[300px] object-cover"></img>
+                    <div class="name pl-3 pt-3 text-2xl text-color6">${data.meals[i].strMeal}</div>
+                    <div class="description pl-3 pb-3 text-color6">${data.meals[i].strArea}, ${data.meals[i].strCategory}</div>
+                </div>
+            `
+        }
     }
-
-    console.log(data.meals[0]);
+    else{
+        document.getElementById('empty-text').innerHTML = 'Nothing was found with this keyword'
+        console.log("empty");
+    }
 }
 
 // loadMeals();
